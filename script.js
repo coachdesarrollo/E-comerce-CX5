6336680dceaaa2471b40d8604a00510a46b5eddc
Thiago Guedes <guedes86@gmail.com>
2024-12-03 19:29:54 -0500

    teste

---
+++ script.js
@@ -0,0 +1,35 @@
+let productos = [
+    {
+        nombre: "Camiseta Negra",
+        precio: "$ 20.00 ",
+        descripcion: " Camiseta Negra Rock Roll ",
+        imagem: " XXXX " 
+    },
+    {
+        nombre: "Camiseta Blanca",
+        precio: "$ 15.00",
+        descripcion: "Camiste Blanca",
+        imagem: " XXXX "
+    },
+    {
+        nombre: "Camiseta Verde ",
+        precio: "$ 20.00 ",
+        descripcion: "Camiseta Verde ",
+        imagem: "XXXX "
+    }
+
+];
+
+function mostrarProductos(){
+    const catalogo = document.getElementById("catalogo")
+}
+
+productos.forEach(function(camisetas){
+
+    const tarjetaProductos = document.createElement("div")
+    tarjetaProductos.classList.add("producto")
+
+
+
+})
+
